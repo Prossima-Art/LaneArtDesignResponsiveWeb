44c7543c621d6f22f98a193bf6a5ae03386dd8ee
alanny-design <alannylucena@gmail.com>
2022-03-21 17:50:54 -0300

    updates

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
     const {t}= useTranslation();
@@ -10,6 +11,7 @@ const PageNotFound = () => {
                 <Row className="errortext">
                 <h3><span className="light">O</span><span className="medium">O</span><span className="bold">O</span><span>ps!</span></h3>
                 <h3>{t('error.title')}</h3>
+                <link to="/">{t('nav_bar_Home')}</link>
                 </Row>
             </Container>
         
