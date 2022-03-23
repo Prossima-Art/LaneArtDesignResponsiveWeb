95687cebfe8cc13113d54e9c0563b42aadc71e94
alanny-design <alannylucena@gmail.com>
2022-03-22 23:54:10 -0300

    hashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 //___
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import { GlobalStyle } from './Styles/GlobalStyles';
 
