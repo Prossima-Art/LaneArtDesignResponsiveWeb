650d28e30916c858986996f983b0b8f51d932830
alanny-design <alannylucena@gmail.com>
2022-03-24 12:18:34 -0300

    cardsizes

--- src/components/carouselportfolio/carouselportfolio.jsx
+++ src/components/carouselportfolio/carouselportfolio.jsx
@@ -1,54 +1,49 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import Button from 'react-bootstrap/Button';
-import {Container, Col, Row, Stack} from 'react-bootstrap';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Button from "react-bootstrap/Button";
+import { Container, Col, Row, Stack } from "react-bootstrap";
 import { motion } from "framer-motion";
 
-import Tamb1 from './imgcarouselportfolio/tamb-home1.png';
-import Tamb2 from './imgcarouselportfolio/tamb-home2.png';
-import Tamb3 from './imgcarouselportfolio/tamb-home3.png';
+import Tamb1 from "./imgcarouselportfolio/tamb-home1.png";
+import Tamb2 from "./imgcarouselportfolio/tamb-home2.png";
+import Tamb3 from "./imgcarouselportfolio/tamb-home3.png";
 
-import './carouselportfolio.css';
-
-// export default function Tambs() {
-//    return (
-//  		<>
-//  			<div>
-//  	 			<a href="#" className="row row-cols-auto">
-//  	 						<motion.div initial={{x:100}} animate={{ x:0 }}><img className="tambs" src= {Tamb1} alt="..."/> </motion.div>
-//  	 				</a>
-//  	 		</div>
-//  	</>		
-//    );
-//  }
-
-
-
+import "./carouselportfolio.css";
 
 export default function Tambs() {
-   return (
- 		<>
-			<div className="clearfix">
-			  				
-			  <div className="img-container2">
-			  <a href="#">
- 	 						<motion.div initial={{x:100 }} animate={{ y:120, x:0 }} transition={{duration: 1, delay: 1}} ><img className=" float" src= {Tamb2} alt="..."/> </motion.div>
- 	 				</a>
-			  </div>
-				
-			  <div className="img-container3">
-			 <a href="#">
- 	 						<motion.div initial={{x:-100 }} animate={{x:0, y:120 }} transition={{ duration: 1, delay: 1}}><img className=" float" src= {Tamb3} alt="..."/> </motion.div>
- 	 				</a>
-			  </div>
+  return (
+    <>
+      <div className="clearfix">
+        <div className="img-container2">
+          <motion.div
+            initial={{ x: 100 }}
+            animate={{ y: 120, x: 0 }}
+            transition={{ duration: 1, delay: 1 }}
+          >
+            <img className=" float size" src={Tamb2} alt="..." />{" "}
+          </motion.div>
+        </div>
 
-				<div className="img-container">
-			  <a href="#">
- 	 						<motion.div initial={{y:-100}} animate={{ y:0 }} transition={{ duration: 1, delay: 1}}><img className=" float" src= {Tamb1} alt="..."/> </motion.div>
- 	 				</a>
-			  </div>
-			</div>
+        <div className="img-container3">
+          <motion.div
+            initial={{ x: -100 }}
+            animate={{ x: 0, y: 120 }}
+            transition={{ duration: 1, delay: 1 }}
+          >
+            <img className=" float size" src={Tamb3} alt="..." />{" "}
+          </motion.div>
+        </div>
 
- 	</>		
-   );
- }
+        <div className="img-container">
+          <motion.div
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 1, delay: 1 }}
+          >
+            <img className=" float size" src={Tamb1} alt="..." />{" "}
+          </motion.div>
+        </div>
+      </div>
+    </>
+  );
+}
