4c6c2215ca32431f70331e2a9670fa4ac4b4820b
neo0112358 <borjaruizreverter@gmail.com>
2023-02-12 16:28:09 -0300

    Formating document

--- src/components/pricegrid/pricegrid.jsx
+++ src/components/pricegrid/pricegrid.jsx
@@ -1,71 +1,76 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import { useTranslation } from 'react-i18next';
-import {Container,Row,Col} from 'react-bootstrap';
-import './pricegrid.css'
-import { ModalContato } from '../modalFrom/modal'//lane
-
-
-
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { useTranslation } from "react-i18next";
+import { Container, Row, Col } from "react-bootstrap";
+import "./pricegrid.css";
+import { ModalContato } from "../modalFrom/modal"; //lane
 
 export function InfoTituloBuy() {
-	const {t}= useTranslation();
+  const { t } = useTranslation();
 
-	return (
-		  <>
-	  <h1 className="h1-md mt-5"> {t('pricesection.title')}</h1>
-	  <p className="p-subtitle textbox">{t('pricesection.subtitle')}</p>   
-	  </>
-	);
-  }
-
-
-
+  return (
+    <>
+      <h1 className="h1-md mt-5"> {t("pricesection.title")}</h1>
+      <p className="p-subtitle textbox">{t("pricesection.subtitle")}</p>
+    </>
+  );
+}
 
 export function Gridprice() {
-	const {t}= useTranslation();
+  const { t } = useTranslation();
   return (
-		<>
-		<Container className=" mt-5">
-			  <Row>				
-			    
-					
-					<Row className="mt-5 centralized">
-						<Col sm={3} className="cards">
-						<p className="p-title mt-3">{t('packages.Desing')}</p>
-						<p className="p-subtitle centralized">{t('packages.item1')}</p>
-						<p className="p-subtitle centralized">{t('packages.item2')}</p>
-						<p className="p-subtitle centralized">{t('packages.item3')}</p>
-						<p className="p-subtitle centralized opacity-1">{t('packages.item4')}</p>
-						<p className="p-subtitle centralized opacity-1">{t('packages.item5')}</p>
-						<p className="p-subtitle centralized opacity-1 mb-5">{t('packages.item6')}</p>						
-						</Col>
+    <>
+      <Container className=" mt-5">
+        <Row>
+          <Row className="mt-5 centralized">
+            <Col sm={3} className="cards">
+              <p className="p-title mt-3">{t("packages.Desing")}</p>
+              <p className="p-subtitle centralized">{t("packages.item1")}</p>
+              <p className="p-subtitle centralized">{t("packages.item2")}</p>
+              <p className="p-subtitle centralized">{t("packages.item3")}</p>
+              <p className="p-subtitle centralized opacity-1">
+                {t("packages.item4")}
+              </p>
+              <p className="p-subtitle centralized opacity-1">
+                {t("packages.item5")}
+              </p>
+              <p className="p-subtitle centralized opacity-1 mb-5">
+                {t("packages.item6")}
+              </p>
+            </Col>
 
-					<Col sm={3} className="cards ">              
-						<p className="p-title mt-3">{t('packages.Dev')}</p>
-						<p className="p-subtitle centralized opacity-1">{t('packages.item1')}</p>
-						<p className="p-subtitle centralized opacity-1">{t('packages.item2')}</p>
-						<p className="p-subtitle centralized opacity-1">{t('packages.item3')}</p>
-						<p className="p-subtitle centralized">{t('packages.item4')}</p>
-						<p className="p-subtitle centralized">{t('packages.item5')}</p>
-						<p className="p-subtitle centralized mb-5">{t('packages.item6')}</p>
-						
-					</Col>
-						
-					<Col sm={3} className="cards">                  
-						<p className="p-title mt-3">{t('packages.Expert')}</p>
-						<p className="p-subtitle centralized">{t('packages.item1')}</p>
-						<p className="p-subtitle centralized">{t('packages.item2')}</p>
-						<p className="p-subtitle centralized">{t('packages.item3')}</p>
-						<p className="p-subtitle centralized">{t('packages.item4')}</p>
-						<p className="p-subtitle centralized">{t('packages.item5')}</p>
-						<p className="p-subtitle centralized mb-5">{t('packages.item6')}</p>
-					</Col>
-					</Row>
-					
-			  </Row>
-	</Container>
-		</>
-		)
+            <Col sm={3} className="cards ">
+              <p className="p-title mt-3">{t("packages.Dev")}</p>
+              <p className="p-subtitle centralized opacity-1">
+                {t("packages.item1")}
+              </p>
+              <p className="p-subtitle centralized opacity-1">
+                {t("packages.item2")}
+              </p>
+              <p className="p-subtitle centralized opacity-1">
+                {t("packages.item3")}
+              </p>
+              <p className="p-subtitle centralized">{t("packages.item4")}</p>
+              <p className="p-subtitle centralized">{t("packages.item5")}</p>
+              <p className="p-subtitle centralized mb-5">
+                {t("packages.item6")}
+              </p>
+            </Col>
+
+            <Col sm={3} className="cards">
+              <p className="p-title mt-3">{t("packages.Expert")}</p>
+              <p className="p-subtitle centralized">{t("packages.item1")}</p>
+              <p className="p-subtitle centralized">{t("packages.item2")}</p>
+              <p className="p-subtitle centralized">{t("packages.item3")}</p>
+              <p className="p-subtitle centralized">{t("packages.item4")}</p>
+              <p className="p-subtitle centralized">{t("packages.item5")}</p>
+              <p className="p-subtitle centralized mb-5">
+                {t("packages.item6")}
+              </p>
+            </Col>
+          </Row>
+        </Row>
+      </Container>
+    </>
+  );
 }
